45d7992f001a472ca09684fad4a97a3b32fe88e3
Szymon Kowaliński <szymon@kowalinski.dev>
2024-05-29 23:53:23 +0200

    feat: better directus client

---
+++ directus_push/client.py
@@ -0,0 +1,8 @@
+import os
+
+from directus_sdk_py import DirectusClient
+
+from dotenv import load_dotenv
+
+load_dotenv()
+client = DirectusClient(url=os.getenv('DIRECTUS_URL'), token=os.getenv('DIRECTUS_TOKEN'))
